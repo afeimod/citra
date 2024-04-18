4005193813668666108702cae6df67f37dceef74
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2024-04-18 15:39:44 +0200

    Revert "Minor fix"
    Broke statesave load...

--- src/core/core.h
+++ src/core/core.h
@@ -422,7 +422,7 @@ private:
 
     std::atomic_bool is_powered_on{};
 
-    ResultStatus status = ResultStatus::Success;
+    ResultStatus status;
     std::string status_details = "";
     /// Saved variables for reset
     Frontend::EmuWindow* m_emu_window;
